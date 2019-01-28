c23fd7afe5339097365cccfc384ed970bfaeb69b
srija-sagi <45347878+srija-sagi@users.noreply.github.com>
2019-01-28 22:18:45 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,80 +1,83 @@
 <html>
 <head>
-<style>
+ <style>
 
 table, th, td {
- border: 1px solid black;
-
-border-collapse: collapse;
+  border: 1px solid black;
+ 
+ border-collapse: collapse;
 }
 
 </style>
 </head>
 <body>
 <?php
-$name=$_GET["name"];
-$gender=$_GET["gender"];
-$address=$_GET["address"];
-$pno=$_GET["pno"];
-$ptype=$_GET["paymenttype"];
-$q1=$_GET["Q1"];
-$q2=$_GET["Q2"];
-$q3=$_GET["Q3"];
-?>
- <?php echo "Name:".$name;?><br>
-   <?php echo"Gender:". $gender;?><br>
-   <?php echo"Address:".$address;?><br>
-   <?php echo"phonenumber:".$pno;?><br>
-   <?php echo"Type of payment you opted:".$ptype;?><br>
-   <table>
-   <tr>
-   <th>item</th>
-   <th>Qty</th> 
-   <th>cost</th>
-   </tr>
- 
+	$name=$_GET["name"];
+	$gender=$_GET["gender"];
+	$address=$_GET["address"];
+	$pno=$_GET["pno"];
+	$ptype=$_GET["paymenttype"];
+	$q1=$_GET["Q1"];
+	$q2=$_GET["Q2"];
+	$q3=$_GET["Q3"];
+	$q4=$_GET["Q4"];
+	$total=$q1*300+$q2*200+$q3*400+$q4*33000;
+	?>
+    <?php echo "Name:".$name;?><br>
+    <?php echo"Gender:". $gender;?><br>
+    <?php echo"Address:".$address;?><br>
+    <?php echo"phonenumber:".$pno;?><br>
+    <?php echo"Type of payment you opted:".$ptype;?><br>
+    <table>
+    <tr>
+    <th>item</th>
+    <th>Qty</th> 
+    <th>cost</th>
+    </tr>
+  
 <tr>
-   
+    
 <td>USB pendrive</td>
-   
-<td><?php echo $qu1;?></td> 
-   
-<td><?php echo $qu1*300;?></td>
-
-</tr>
-
-<tr>
-  
-<td>wireless mouse</td>
-  
-<td><?php echo $qu2;?></td> 
-  
-<td><?php echo $qu2*200;?></td>
+    
+<td><?php echo $q1;?></td> 
+    
+<td><?php echo $q1*300;?></td>
+ 
  </tr>
-
-<tr>
-  
-<td>keyboard</td>
-  
-<td><?php echo $qu3;?></td> 
+ 
+ <tr>
    
-<td><?php echo $qu3*400;?></td>
-
-</tr>
-
-<tr>
+ <td>wireless mouse</td>
    
+ <td><?php echo $q2;?></td> 
+   
+ <td><?php echo $q2*200;?></td>
+  </tr>
+ 
+ <tr>
+   
+ <td>keyboard</td>
+   
+ <td><?php echo $q3;?></td> 
+    
+<td><?php echo $q3*400;?></td>
+ 
+ </tr>
+ 
+ <tr>
+    
 <td>laptop</td>
-   
-<td><?php echo $qu4;?></td> 
-   
-<td><?php echo $qu4*33000;?></td>
-
-</tr>
-<tr>
-<td>Total cost:$qu1**300+$qu2*200+$qu3*400+$qu4*33000;</td>
+    
+<td><?php echo $q4;?></td> 
+    
+<td><?php echo $q4*33000;?></td>
+ 
+ </tr>
+ <tr>
+<td><?php echo "Total cost:";?></td>
+<td><?php echo $total;?></td>
 </tr>
 </table>
-echo"thanks for your order";
+thanks for your order
 </body>
 </html>
